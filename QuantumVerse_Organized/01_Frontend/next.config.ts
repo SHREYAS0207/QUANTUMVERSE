4d7f7cf43d9ea637f9dc1efc45b2e85208003cb4
SHREYAS0207 <shreyas02.kulkarni@gmail.com>
2026-09-21 08:21:45 +0000

    Fix Vercel standalone deployment

--- QuantumVerse_Organized/01_Frontend/next.config.ts
+++ QuantumVerse_Organized/01_Frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",           // Needed for Docker
+  output: process.env.VERCEL ?
+  undefined : "standalone" ,           // Needed for Docker
   poweredByHeader: false,
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
